cacca157bcc467d235f0d96164b8f2b6bf469515
SUSTech-Cooper <12110517@mail.sustech.edu.cn>
2022-06-15 23:19:54 +0800

    calculation in addition

---
+++ src/Calculation.java
@@ -0,0 +1,25 @@
+public class Calculation {
+    public static void main(String[] args) {
+        int[][] A={{1,2,3},{3,4,5}};
+        int[][] B={{5,6,7},{4,5,9}};
+        print(add(A,B));
+    }
+
+    public static int[][] add(int[][] a, int[][] b){
+        int[][] result = new int[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                result[i][j]=a[i][j]+b[i][j];
+            }
+        }
+        return result;
+    }
+    public static void print(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.printf("%d ",arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
